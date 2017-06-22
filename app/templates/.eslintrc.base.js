63aac634838b2c1bdcf9788ba82c04b1dd29c188
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-06-22 12:21:53 -0400

    Update eslint template

--- app/templates/.eslintrc.base.js
+++ app/templates/.eslintrc.base.js
@@ -5,7 +5,6 @@ module.exports = {
     es6: true,
   },
   'extends': ['eslint:recommended', 'plugin:react/recommended'],
-  'installedESLint': true,
   'parser': 'babel-eslint',
   'parserOptions': {
     'ecmaFeatures': {
